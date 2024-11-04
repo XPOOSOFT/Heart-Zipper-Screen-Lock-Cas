bd623eb03979c9d1dbc3de52c2b20bab4b48388d
Xpoosoft <javedkhankhastech@gmail.com>
2024-11-04 17:43:53 +0500

    Home

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.heartzipperlock.lovezipper.romanticlockscreen.securelock.roselock"
         minSdk = 24
         targetSdk = 34
-        versionCode = 56
-        versionName = "23.6"
+        versionCode = 57
+        versionName = "23.7"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         setProperty("archivesBaseName", "LoveHeartLockScreen_v$versionName($versionCode)")
     }
